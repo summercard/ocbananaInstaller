be8c735618bb2d97a071dc9bcdeaa7aee650e275
zhuangmenghong <summercards@gmail.com>
2026-02-28 11:07:10 +0800

    fix: 处理requests未安装的情况

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,11 +4,19 @@
 
 from .platform import Platform
 from .logger import Logger, get_logger
-from .downloader import Downloader
 
-__all__ = [
-    'Platform',
-    'Logger',
-    'get_logger',
-    'Downloader'
-]
+try:
+    from .downloader import Downloader
+    __all__ = [
+        'Platform',
+        'Logger',
+        'get_logger',
+        'Downloader'
+    ]
+except ImportError:
+    # requests 未安装时跳过
+    __all__ = [
+        'Platform',
+        'Logger',
+        'get_logger',
+    ]
