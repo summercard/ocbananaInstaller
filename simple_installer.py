be68593789bc2a658e494b7046406c21fff2cc7e
summercards <summercards@gmail.com>
2026-03-03 08:56:24 +0800

    v4: 修复预设任务输入框为空的问题

--- simple_installer.py
+++ simple_installer.py
@@ -537,8 +537,9 @@ class OpenClawApp:
             row_frame = ttk.Frame(claude_frame)
             row_frame.pack(fill=tk.X, padx=10, pady=5)
             
-            # 输入框
-            entry = ttk.Entry(row_frame, textvariable=tk.StringVar(value=task))
+            # 输入框 - 使用 tk.Entry 而不是 ttk.Entry
+            var = tk.StringVar(value=task)
+            entry = tk.Entry(row_frame, textvariable=var, font=('Helvetica', 10))
             entry.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=(0, 5))
             
             # 按钮
